09223714e1d5ccb45977467cfb57a6be19842678
cqupt-zjs <1250656856@qq.com>
2015-05-25 18:50:42 +0800

    vffggf

---
+++ src/Person.java
@@ -0,0 +1,18 @@
+
+public class Person {
+	private String name;
+	private String psw;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+	
+}
